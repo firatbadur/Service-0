cae68dbc3877b807c0ea2377e44f16d05bf40ac1
Fırat Badur <firatbadur@outlook.com>
2020-10-20 11:13:48 +0300

    *

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import db
 from flask_apscheduler import APScheduler
 import instantaneous
 from data import json_data
-
+print("Hello World")
 realtime_ascii = []
 realtime_modbus = []
 
@@ -22,7 +22,7 @@ siteID = station_details[0]
 auth_user = station_details[4]
 auth_pass = station_details[5]
 
-
+xvbvbcvb
 app = Flask(__name__)
 scheduler = APScheduler()
 
